8f70c78ea8750903552a6588fea5f5175fc58349
rb9181 <140580201+rb9181@users.noreply.github.com>
2025-07-14 06:32:13 +0000

    edited

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ import joblib
 
 app = Flask(__name__)
 
-@app.route("/prediction",methods=["GET","POST"])
-def prediction():
-    q = float(request.form.get("q"))
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
 
-    # load model
-    model = joblib.load("dbs.jl")
+@app.route("/dbs",methods=["GET","POST"])
+def idbs():
+    return(render_template("dbs.html"))
 
-    # make prediction
-    pred = model.predict([[q]])
+@app.route("/main,methods=["GET","POST"])
+def main():
+    q = (request.form.get("q")
 
-    return(render_template("prediction.html",r=pred))
+    # db
+    return(render_template("main.html",r=pred))
            
 if __name__ == "__main__":
     app.run()
