68af58d97a22da38cf2831e6fdc3a248ee8ed4a0
rb9181 <140580201+rb9181@users.noreply.github.com>
2025-07-14 04:03:09 +0000

    change to joblib

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def prediction():
     # make prediction
     pred = model.predict([[q]])
 
-    return(render_template("prediction.html",r=pred)
+    return(render_template("prediction.html",r=pred))
+           
 if __name__ == "__main__":
     app.run()
